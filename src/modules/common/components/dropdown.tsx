13775da44c7e834000261938ae07229689599fda
Lucas Levandoski <lucas.levandoski@bp.com>
2024-10-30 09:25:50 -0300

    adding an opacity transition to the dropdown

--- src/modules/common/components/dropdown.tsx
+++ src/modules/common/components/dropdown.tsx
@@ -62,9 +62,9 @@ export function Select({ id, children, value, className, placeholder = '', onCha
     <div id={id} className={twMerge('relative w-full flex h-12', className)} ref={divRef}>
       <div className={
         twMerge(
-          'transition-all duration-300 absolute flex flex-col w-full max-h-0 overflow-auto translate-y-14 z-10', 
+          'transition-all duration-300 absolute flex flex-col w-full max-h-0 overflow-auto opacity-0 translate-y-14 z-10', 
           'rounded-lg bg-white shadow-lg',
-          isOpen && 'max-h-52'
+          isOpen && 'max-h-52 opacity-100'
         )
       }>
         <div className="flex flex-col h-full">
