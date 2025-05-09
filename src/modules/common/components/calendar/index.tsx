205a04d4b9d2ac2f85db0edfc60a3102c8956b96
Lucas Levandoski <lucas.levandoski@bp.com>
2025-05-09 16:34:20 -0300

    removing ability to translate calendar

--- src/modules/common/components/calendar/index.tsx
+++ src/modules/common/components/calendar/index.tsx
@@ -41,7 +41,7 @@ export function Calendar({
 }: props) {
 
   return (
-    <div className="shadow-lg rounded-xl pt-6 h-fit">
+    <div translate="no" className="shadow-lg rounded-xl pt-6 h-fit">
       <LocalizationProvider 
         adapterLocale="pt-br"
         dateAdapter={AdapterDayjs}  
